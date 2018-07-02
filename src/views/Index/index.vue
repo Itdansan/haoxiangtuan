5b92c44991c4beb4592fea9509e58d8b4487c381
JiaQiHuang <304289973@qq.com>
2018-07-03 00:43:58 +0800

    首次提交代码

---
+++ src/views/Index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="index">
+		<p>我是首页</p>
+		<div class="testDiv"></div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+	.index{
+		width: 100%;
+		.testDiv{
+			width: 500px;
+			height: 500px;
+			background-color: red;
+		}
+	}
+</style>
